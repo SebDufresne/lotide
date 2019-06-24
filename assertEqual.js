5fb4c03b6be8fb1fe5907d7aa9b35e09a09bb1d2
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-06-24 19:55:30 +0000

    Updated to use ES6 Template Literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   actual === expected ?
-    console.log('\u2705\u2705\u2705 ' + 'Assertion Passed: "' + actual + '" === "' + expected + '"') :
-    console.log("\uD83D\uDD34\uD83D\uDD34\uD83D\uDD34 " + 'Assertion Failed: "' + actual + '" !== "' + expected + '"');
+    console.log(`\u2705 Assertion Passed: "${actual}" === "${expected}"`) :
+    console.log(`\uD83D\uDD34 Assertion Failed: "${actual}" !== "${expected}"`);
 };
 
 // TEST CODE
