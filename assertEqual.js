c338bceb806d46bdcabe04625526e9d17f3e423e
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-06-24 19:48:35 +0000

    Created assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  actual === expected ?
+    console.log('\u2705\u2705\u2705 ' + 'Assertion Passed: "' + actual + '" === "' + expected + '"') :
+    console.log("\uD83D\uDD34\uD83D\uDD34\uD83D\uDD34 " + 'Assertion Failed: "' + actual + '" !== "' + expected + '"');
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
