227639974f2b282939430183176bdb5ab895268d
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-06-25 20:44:50 +0000

    Added without.js

---
+++ without.js
@@ -0,0 +1,27 @@
+const assertArraysEqual = function(actual, expected) {
+  JSON.stringify(actual) === JSON.stringify(expected) ?
+    console.log(`\u2705 Assertion Passed: "${actual}" === "${expected}"`) :
+    console.log(`\uD83D\uDD34 Assertion Failed: "${actual}" !== "${expected}"`);
+};
+
+const eqArrays = function(arr1, arr2) {
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
+};
+
+const without = function(arr,withoutElems) {
+  let returnArr = [];
+  arr.forEach((element) => {
+    if (!withoutElems.includes(element)) {
+      returnArr.push(element);
+    }
+  });
+  return returnArr;
+};
+
+assertArraysEqual(without(["hello", "world", "lighthouse"], ["lighthouse"]), ["hello", "world"]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]),["1", "2"]);
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
