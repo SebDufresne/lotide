e8f52752cd649630efa91ce4352a309ddb504740
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-06-27 19:39:54 +0000

    Added inspect to assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,8 @@
 const assertArraysEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
   JSON.stringify(actual) === JSON.stringify(expected) ?
-    console.log(`\u2705 Assertion Passed: "${actual}" === "${expected}"`) :
-    console.log(`\uD83D\uDD34 Assertion Failed: "${actual}" !== "${expected}"`);
+    console.log(`\u2705 Assertion Passed: "${inspect(actual)}" === "${inspect(expected)}"`) :
+    console.log(`\uD83D\uDD34 Assertion Failed: "${inspect(actual)}" !== "${inspect(expected)}"`);
 };
 
 //assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
