2b8dacc0e4dfd6e108bfa91a45a97fad4646714f
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-06-28 23:37:20 +0000

    Made eqArrays.js recursive

--- eqArrays.js
+++ eqArrays.js
@@ -5,10 +5,21 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  return JSON.stringify(arr1) === JSON.stringify(arr2);
+  return arr1.length === arr2.length &&
+         arr1.reduce((acc,ele,i) => Array.isArray(ele) ? eqArrays(ele,arr2[i]) : ele === arr2[i],true);
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+
+
+
+console.log(eqArrays([[2, 3], [4],[[[1],2]]],[[2, 3], [4],[[[1],2]]])); // => true
