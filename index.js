144bef4a0f63c30600c405a0edbfd89efa5f8b05
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-01 23:12:41 +0000

    Updated map to unit test with mocha

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const head = require('./head');
-const tail = require('./tail');
-const middle = require('./middle');
+const head      = require('./head');
+const map       = require('./map');
+const middle    = require('./middle');
+const tail      = require('./tail');
 const takeUntil = require('./takeUntil');
-const without = require('./without');
+const without   = require('./without');
 
 module.exports = {
   head: head,
-  tail: tail,
+  map: map,
   middle: middle,
+  tail: tail,
   takeUntil: takeUntil,
   without: without,
 };
--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -2,16 +2,16 @@ const assert = require('chai').assert;
 const takeUntil = require('../index').takeUntil;
 
 describe('#takeUntil', () =>{
-  it("returns [1, 2] for [1, 2, -1, 4] with callback x => x < 0", () =>{
+  it("returns [1, 2] for [1, 2, -1, 4] with callback: x => x < 0", () =>{
     const input = [1, 2, -1, 4];
-    const expected = [1, 2];
-    const callbackFct = x => x < 0;
-    assert.deepEqual(takeUntil(input, callbackFct),expected);
+    const expect = [1, 2];
+    const callback = x => x < 0;
+    assert.deepEqual(takeUntil(input, callback),expect);
   });
   it("returns [ 'Hey' ] for ['Hey', '!','How','are','you','?'] with callback x => x === '!'", () => {
     const input = ['Hey', '!','How','are','you','?'];
-    const expected = [ 'Hey' ];
-    const callbackFct = x => x === '!';
-    assert.deepEqual(takeUntil(input,callbackFct),expected);
+    const expect = [ 'Hey' ];
+    const callback = x => x === '!';
+    assert.deepEqual(takeUntil(input,callback),expect);
   });
 });
