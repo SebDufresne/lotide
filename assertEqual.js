34f271edf75b4f2522812fb13ad213447834771a
Sebastien Dufresne <splitul@gmail.com>
2019-07-01 14:50:52 -0400

    Refactored head.js and assertEqual.js to use external test bench

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,7 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   actual === expected ?
     console.log(`\u2705 Assertion Passed: "${actual}" === "${expected}"`) :
     console.log(`\uD83D\uDD34 Assertion Failed: "${actual}" !== "${expected}"`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,13 +1,5 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ?
-    console.log(`\u2705 Assertion Passed: "${actual}" === "${expected}"`) :
-    console.log(`\uD83D\uDD34 Assertion Failed: "${actual}" !== "${expected}"`);
-};
-
 const head = function(arr) {
   return arr[0] ? arr[0] : undefined;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log(head([]));
+module.exports = head;
