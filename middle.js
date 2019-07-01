df21549fa18ac13860f15ee996a32094af1aa675
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-01 21:34:57 +0000

    Refactoring middle

--- middle.js
+++ middle.js
@@ -1,11 +1,11 @@
 const middle = function(array) {
-  if (array.length < 3) {
-    return [];
-  }
-  let middleArr = [];
-  middleArr.push(array[Math.ceil(array.length / 2) - 1]);
+  if (array.length < 3) return [];
+
+  const midPoint = Math.ceil(array.length / 2);
+  const middleArr = [];
+  middleArr.push(array[Math.ceil(midPoint) - 1]);
   if (!(array.length % 2)) {
-    middleArr.push(array[Math.ceil(array.length / 2)]);
+    middleArr.push(array[Math.ceil(midPoint)]);
   }
   return middleArr;
 };
