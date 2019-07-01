b6d84a14112e21d5e82fe0a61983ec4ffad88446
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-01 22:33:20 +0000

    Updated takeUntil to unit test with mocha

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 module.exports = {
   head: head,
   tail: tail,
   middle: middle,
+  takeUntil: takeUntil,
   without: without,
 };
--- takeUntil.js
+++ takeUntil.js
@@ -1,10 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  JSON.stringify(actual) === JSON.stringify(expected) ?
-    console.log(`\u2705 Assertion Passed: "${inspect(actual)}" === "${inspect(expected)}"`) :
-    console.log(`\uD83D\uDD34 Assertion Failed: "${inspect(actual)}" !== "${inspect(expected)}"`);
-};
-
 const takeUntil = function(array, callback) {
   const returnArr = [];
   for (const ele of array) {
@@ -17,22 +10,4 @@ const takeUntil = function(array, callback) {
   return returnArr;
 };
 
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-assertArraysEqual(takeUntil(data1, x => x < 0),[ 1, 2, 5, 7, 2 ]);
-assertArraysEqual(takeUntil(data2, x => x === ','),[ 'I\'ve', 'been', 'to', 'Hollywood' ]);
-
-/*
-[ 1, 2, 5, 7, 2 ]
----
-[ 'I\'ve', 'been', 'to', 'Hollywood' ]
-*/
+module.exports = takeUntil;
