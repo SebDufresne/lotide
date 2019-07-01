ea6646a7a3d239fed6e1fb0abda2888943c26cd2
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-01 19:51:45 +0000

    Refactored middle.js to use external test file

--- middle.js
+++ middle.js
@@ -1,12 +1,4 @@
-const assertArraysEqual = function(actual, expected) {
-  JSON.stringify(actual) === JSON.stringify(expected) ?
-    console.log(`\u2705 Assertion Passed: "${actual}" === "${expected}"`) :
-    console.log(`\uD83D\uDD34 Assertion Failed: "${actual}" !== "${expected}"`);
-};
-
-const eqArrays = function(arr1, arr2) {
-  return JSON.stringify(arr1) === JSON.stringify(arr2);
-};
+// middle.js
 
 const middle = function(array) {
   if (array.length < 3) {
@@ -20,12 +12,4 @@ const middle = function(array) {
   return middleArr;
 };
 
-
-assertArraysEqual(middle([1]),[]); // => []
-assertArraysEqual(middle([1, 2]),[]); // => []
-
-assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]); // => [3, 4]
+module.exports = middle;
